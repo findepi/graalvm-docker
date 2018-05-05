1ce17b333f7dab5cdb2ab48f28be62feef492791
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-05-06 01:09:35 +0200

    Publish :latest-polyglot

--- bin/build.sh
+++ bin/build.sh
@@ -24,5 +24,6 @@ docker build \
     --build-arg "GRAAL_VERSION=${GRAAL_VERSION}" \
     --cache-from "${IMAGE_NAME}:${GRAAL_VERSION}-polyglot" \
     --tag "${IMAGE_NAME}:${GRAAL_VERSION}-polyglot" \
+    --tag "${IMAGE_NAME}:latest-polyglot" \
     --tag "local-graalvm-polyglot:latest" \
     .

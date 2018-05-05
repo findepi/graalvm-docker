cb0ef13377395378d8798ccfdedb4959038fd696
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-05-06 00:56:32 +0200

    Use caching for continuous deployment
    Explanation https://medium.com/mobileforgood/coding-tips-patterns-for-continuous-integration-with-docker-on-travis-ci-9cedb8348a62#5d35

--- bin/build.sh
+++ bin/build.sh
@@ -5,17 +5,24 @@ set -veuo pipefail
 cd "$(dirname "$0")/.."
 source ./settings.sh
 
+docker pull "${IMAGE_NAME}:${GRAAL_VERSION}" || true
+docker pull "${IMAGE_NAME}:${GRAAL_VERSION}-polyglot" || true
+
 docker build \
+    --pull \
     --file Dockerfile \
     --build-arg "GRAAL_VERSION=${GRAAL_VERSION}" \
-    --tag "${IMAGE_NAME}:latest" \
+    --cache-from "${IMAGE_NAME}:${GRAAL_VERSION}" \
     --tag "${IMAGE_NAME}:${GRAAL_VERSION}" \
+    --tag "${IMAGE_NAME}:latest" \
     --tag "local-graalvm:latest" \
     .
 
 docker build \
+    --pull \
     --file Dockerfile.polyglot \
     --build-arg "GRAAL_VERSION=${GRAAL_VERSION}" \
+    --cache-from "${IMAGE_NAME}:${GRAAL_VERSION}-polyglot" \
     --tag "${IMAGE_NAME}:${GRAAL_VERSION}-polyglot" \
     --tag "local-graalvm-polyglot:latest" \
     .
--- settings.sh
+++ settings.sh
@@ -1,4 +1,3 @@
 #!bash
 
-IMAGE_NAME="findepi/graalvm"
 GRAAL_VERSION=1.0.0-rc1

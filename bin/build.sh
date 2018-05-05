5f86f7b4ec811363af2b1f1851bbef5aedc16a24
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-05-06 00:31:40 +0200

    Build & deploy latest tag

--- bin/build.sh
+++ bin/build.sh
@@ -8,6 +8,7 @@ source ./settings.sh
 docker build \
     --file Dockerfile \
     --build-arg "GRAAL_VERSION=${GRAAL_VERSION}" \
+    --tag "${IMAGE_NAME}:latest" \
     --tag "${IMAGE_NAME}:${GRAAL_VERSION}" \
     --tag "local-graalvm:latest" \
     .

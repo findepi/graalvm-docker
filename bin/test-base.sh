347fb94990731b7bbf19ce601048af1a30396355
Piotr Findeisen <piotr.findeisen@gmail.com>
2018-07-03 21:28:47 +0200

    Show java version when testing

--- bin/test-base.sh
+++ bin/test-base.sh
@@ -2,4 +2,4 @@
 
 set -xeuo pipefail
 
-docker run --rm "${IMAGE_NAME}:latest" java -version 2>&1 | grep GraalVM
+docker run --rm "${IMAGE_NAME}:latest" java -version 2>&1 | tee /dev/stderr | grep GraalVM

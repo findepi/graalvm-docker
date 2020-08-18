e23aa2c961bc9323b2386f85a1afc404e466c718
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-08-18 23:50:15 +0200

    Upgrade to 20.2.0

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 #!bash
 
 export IMAGE_NAME=findepi/graalvm # by necessity, this is also set in derived images' FROM directive
-export GRAAL_VERSION="20.1.1"
+export GRAAL_VERSION="20.2.0"
 export JDK_VERSION="${JDK_VERSION-java8}"
 export DEFAULT="${DEFAULT-false}"

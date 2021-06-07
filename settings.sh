ae9c2fb2785af64c5f8bba823b0f507c98a43a1c
davidfrickert <david.frickert@protonmail.com>
2021-06-07 20:04:27 +0100

    Support Graal Version 21.0.0

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 #!bash
 
 export IMAGE_NAME=findepi/graalvm # by necessity, this is also set in derived images' FROM directive
-export GRAAL_VERSION="21.0.0.2"
+export GRAAL_VERSION="21.1.0"
 export JDK_VERSION="${JDK_VERSION-java8}"
 export DEFAULT="${DEFAULT-false}"

a99fca21d1de4860f359c9bea5423cff218278d0
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-10-25 21:50:09 +0200

    Upgrade to 22.3.0

--- bin/test-polyglot.sh
+++ bin/test-polyglot.sh
@@ -2,7 +2,13 @@
 
 set -xeuo pipefail
 
+image="${IMAGE_NAME}:${GRAAL_VERSION}-${JDK_VERSION}-polyglot"
+graalvm_python="python"
+
+# Verify graalvm_python is indeed GraalVM Python
+docker run -i --rm  "${image}" "${graalvm_python}" --version | grep 'GraalVM Python'
+
 echo "print(2**42)" > /tmp/test-script.py
-docker run -i --rm -v /tmp/test-script.py:/tmp/test-script.py "${IMAGE_NAME}:${GRAAL_VERSION}-${JDK_VERSION}-polyglot" graalpython /tmp/test-script.py \
+docker run -i --rm -v /tmp/test-script.py:/tmp/test-script.py "${image}" ${graalvm_python} /tmp/test-script.py \
     | tee /dev/stderr \
     | grep -xF 4398046511104

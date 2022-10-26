768784d5c961783c68f988832d3069acf5667b34
Piotr Findeisen <piotr.findeisen@gmail.com>
2022-10-26 09:56:53 +0200

    Test Java version declared by base image
    Verify it's what it should be.

--- bin/test-base.sh
+++ bin/test-base.sh
@@ -2,4 +2,7 @@
 
 set -xeuo pipefail
 
-docker run --rm "${IMAGE_NAME}:${GRAAL_VERSION}-${JDK_VERSION}" java -version 2>&1 | tee /dev/stderr | grep GraalVM
+image="${IMAGE_NAME}:${GRAAL_VERSION}-${JDK_VERSION}"
+docker run --rm "${image}" java -version 2>&1
+docker run --rm "${image}" java -version 2>&1 | grep GraalVM
+docker run --rm "${image}" java -version 2>&1 | grep "openjdk version \"${JDK_VERSION#java}."
